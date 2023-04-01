881085ee713db69528eb085025793b396829efeb
Abhisheklikho <111559519+Abhisheklikho@users.noreply.github.com>
2023-04-01 21:08:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+for (let i = 0; i < 5; i++) {
+  const time = Math.floor(Math.random() * 5) + 1; // random time between 1 and 5 seconds
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Promise ${i+1} resolved after ${time} seconds`);
+    }, time * 1000);
+  });
+  promises.push(promise);
+}
